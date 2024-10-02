2581496b0882382a11a212e88f6edcc2b42d9adb
hiteshnatha15 <hiteshnatha15@gmail.com>
2024-10-02 14:30:16 +0530

    admin middleware updated

--- backend/middlewares/adminAuth.js
+++ backend/middlewares/adminAuth.js
@@ -3,50 +3,49 @@ const Admin = require("../models/adminModel");
 require("dotenv").config();
 
 const adminAuth = async (req, res, next) => {
-  const token = req.header("Authorization")?.replace("Bearer ", "");
+    const token = req.header("Authorization")?.replace("Bearer ", "");
 
-  if (!token) {
-    return res.status(401).send({ error: "Access denied. No token provided." });
-  }
-
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const admin = await Admin.findById(decoded.id);
-    if (!admin) {
-      return res
-        .status(403)
-        .send({ error: "Access denied. Admin privileges required." });
+    if (!token) {
+        return res.status(401).send({ error: "Access denied. No token provided." });
     }
 
-    req.user = admin;
-    next();
-  } catch (error) {
-    res.status(400).send({ error: "Invalid token." });
-    console.error(error);
-  }
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const admin = await Admin.findById(decoded.id);
+        if (!admin) {
+            return res
+                .status(403)
+                .send({ error: "Access denied. Admin privileges required." });
+        }
+
+        req.user = admin;
+        next();
+    } catch (error) {
+        res.status(400).send({ error: "Invalid token." });
+        console.error(error);
+    }
 };
 
 const verifyAdmin = async (req, res) => {
-  const token = req.header("Authorization")?.replace("Bearer ", "");
+    const token = req.header("Authorization")?.replace("Bearer ", "");
 
-  if (!token) {
-    return res.status(401).send({ error: "Access denied. No token provided." ,success: false});
-  }
-
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const admin = await Admin.findById(decoded.id);
-    if (!admin) {
-      return res
-        .status(403)
-        .send({ error: "Access denied. Admin privileges required.", success: false });
+    if (!token) {
+        return res.status(401).send({ error: "Access denied. No token provided.", success: false });
     }
-    return res.status(200).send({ success: true });
-  } catch (error) {
-    res.status(400).send({ error: "Invalid token." ,success: false});
-    console.error(error);
-  }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const admin = await Admin.findById(decoded.id);
+        if (!admin) {
+            return res
+                .status(403)
+                .send({ error: "Access denied. Admin privileges required.", success: false });
+        }
+        return res.status(200).send({ success: true });
+    } catch (error) {
+        res.status(400).send({ error: "Invalid token.", success: false });
+        console.error(error);
+    }
 };
 
-module.exports = adminAuth;
-module.exports = verifyAdmin;
+module.exports = { adminAuth, verifyAdmin };
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const { auth, verifyUser } = require("../middlewares/auth");
-const adminAuth = require("../middlewares/adminAuth");
-const verifyAdmin = require("../middlewares/adminAuth");
+const {adminAuth} = require("../middlewares/adminAuth");
+const {verifyAdmin} = require("../middlewares/adminAuth");
 
 const {
   adminLogin,
