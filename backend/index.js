56a216e98dde4bb595ce1a8f3943ce424e9e6914
hiteshnatha15 <hiteshnatha15@gmail.com>
2024-10-03 16:48:19 +0530

    backend updated

--- backend/index.js
+++ backend/index.js
@@ -22,10 +22,10 @@ app.use(routes); // Use routes
 // Serve the static files from the frontend directory
 // app.use(express.static(path.join(__dirname, 'frontend')));
 
-// Set the route for your frontend
-app.get('*', (req, res) => {
-   res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
-});
+// // Set the route for your frontend
+// app.get('*', (req, res) => {
+//    res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
+// });
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
